b05da7af4c1396c910a60fc829ea77b4b371be7f
Caregraf <info@caregraf.com>
2015-07-14 21:26:52 -0700

    update to import script and instructions to cover whatever is in ../schemes

--- fuseki/README.md
+++ fuseki/README.md
@@ -3,19 +3,14 @@
   1. Download and unzip Fuseki into this directory (from: http://jena.apache.org/download/#apache-jena-fuseki)
      ... apache-jena-fuseki-2.0.0.zip (July 2015). Rename it to apache-jena-fuseki (ie/ no version information)
 
-  2. Make a directory for your SKS database
- 
-     mkdir sksdb
+  2. Run the 'importSchemes.sh' script. Will take a few minutes but once loaded, you're ready to go.
+     - it will install a database in a new or refreshed directory called 'sksdb'
 
-  3. Run the 'importSchemes.sh' script. Will take a few minutes but once loaded, you're ready to go.
-
-  4. In a new shell, go into the apache-jena-fuseki directory and run fuseki
+  3. In a new shell, go into the apache-jena-fuseki directory and run fuseki
 
      fuseki-server --update --loc ../sksdb /sks
 
-  5. Run 
+  4. To see queries in action, run 
 
      python drugRecipes.py 
-
-     to see queries in action
 
--- fuseki/importSchemes.sh
+++ fuseki/importSchemes.sh
@@ -1,13 +1,31 @@
-#
-# Load available schemes
-# 
-# Reads from zip, pipes through an nquads convertor and then imports into 
-# named graph
-# 
-# For more on Jena i/o read https://jena.apache.org/documentation/io/
-#
-# TODO: loop schemes inferring schemeMN and graph from naming convention  
-#
-
-unzip -p ../schemes/RXNORM_15AA_150601F.zip RXNORM_15AA_150601F/scheme.ttl | java -cp ".:apache-jena-fuseki/fuseki-server.jar" riotcmd.turtle --output nquads | java -cp ".:apache-jena-fuseki/fuseki-server.jar" tdb.tdbloader --loc sksdb --graph http://schemes.caregraf.info/rxnorm -- -
-unzip -p ../schemes/MTHSPL_2015_05_22.zip MTHSPL_2015_05_22/scheme.ttl | java -cp ".:apache-jena-fuseki/fuseki-server.jar" riotcmd.turtle --output nquads | java -cp ".:apache-jena-fuseki/fuseki-server.jar" tdb.tdbloader --loc sksdb --graph http://schemes.caregraf.info/mthspl -- -
+#!/bin/bash
+echo
+DBLOC=sksdb
+mkdir -p $DBLOC
+echo "On $(date +%Y/%m/%d), clearing '${DBLOC}' and (re)loading schemes ..."
+if [ "$(ls -A $DBLOC)" ]; then
+    echo "Clearing current contents of database location '$DBLOC'"
+    rm $DBLOC/*
+fi
+SCHEMEZIPS="../schemes/*zip"
+ZIPRE='\/([A-Z0-9]+)_([^\.]+)\.zip$'
+CP=".:apache-jena-fuseki/fuseki-server.jar"
+for szip in $SCHEMEZIPS; do
+    if [[ $(echo $szip) =~ $ZIPRE ]]; then
+        SCHEMEMN=${BASH_REMATCH[1]}
+        GRAPHID="http://schemes.caregraf.info/$(tr [A-Z] [a-z] <<< "$SCHEMEMN")"
+        VERSION=${BASH_REMATCH[2]}
+        echo 
+        echo "Loading ${SCHEMEMN}, version ${VERSION} into <${GRAPHID}> ..."
+        for i in {1..100}
+        do
+            echo -n '-'
+        done
+        echo
+        TTLINZIP="${SCHEMEMN}_${VERSION}/scheme.ttl"
+        echo $TTLINZIP
+        unzip -p $szip $TTLINZIP | java -cp $CP riotcmd.turtle --output nquads | java -cp $CP tdb.tdbloader --loc $DBLOC --graph $GRAPHID -- -
+    fi
+echo
+echo
+done
